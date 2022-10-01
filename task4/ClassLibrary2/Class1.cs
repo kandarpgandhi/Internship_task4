80b0f072ba18b11f2c4b6db3ee7817f4398d8298
kandarpgandhi <kgandhi2206@gmail.com>
2022-10-01 15:38:34 +0530

    task4

---
+++ task4/ClassLibrary2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public DataSet selectDatabyId(string str)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-GI21EV1\SQLEXPRESS; Initial Catalog=ApiDatabase; Integrated Security=true;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(str,con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            return ds;
+            con.Close();
+        }
+    }
+}
---
+++ task4/WebApplicationAPI2/Controllers/condoctorApiController.cs
@@ -0,0 +1,128 @@
+﻿using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationAPI2.Models;
+using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationAPI2.Controllers
+{
+    public class condoctorApiController : ApiController
+    {
+        ApiDatabaseEntities db = new ApiDatabaseEntities();
+        
+        [HttpGet]
+        public IHttpActionResult Getdata()
+        {
+            Class1 obj=new Class1();
+            DataSet ds = obj.selectDatabyId("spGetCondoctor");
+            var l = ds.Tables[0].AsEnumerable().Select(DataRow => new condoctor { c_id = DataRow.Field<int>("c_id"), c_name = DataRow.Field<string>("c_name"), c_age = DataRow.Field<int>("c_age") }).ToList();
+            return Ok(l);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Getdata(int id)
+        {
+            Class1 obj = new Class1();
+            DataSet ds=obj.selectDatabyId($"spGetCndoctorById {id}");
+            var l =ds.Tables[0].AsEnumerable().Select(DataRow=>new condoctor { c_id = DataRow.Field<int>("c_id"),c_name =DataRow.Field<string>("c_name"), c_age = DataRow.Field<int>("c_age") }).ToList();
+            return Ok(l);
+        }
+
+       [HttpPost]
+        public IHttpActionResult Insertdata(abcTest a)
+        {
+           
+            int id = a.c_id;
+            string name = a.name;
+            int age=a.age;
+            if (a.bus_id != null)
+            {
+                int bId = int.Parse(a.bus_id);
+                common cm = new common();
+                cm.c_id = id;
+                cm.bus_id = bId;
+                db.commons.Add(cm);
+                db.SaveChanges();
+            }
+            condoctor c = new condoctor();
+            c.c_id = id;
+            c.c_name = name;
+            c.c_age = age;
+            db.condoctors.Add(c);
+            db.SaveChanges();
+            return Ok();
+        }
+        [Route("api/condoctorApi/InsertArrdata")]
+        [HttpPost]
+        public IHttpActionResult InsertArrdata(abcTest[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                int id = a[i].c_id;
+                string name = a[i].name;
+                int age = a[i].age;
+
+                if (a[i].bus_id != null)
+                {
+                    int bId = int.Parse(a[i].bus_id);
+                    common cm = new common();
+                    cm.c_id = id;
+                    cm.bus_id = bId;
+                    db.commons.Add(cm);
+                    db.SaveChanges();
+                }
+                condoctor c = new condoctor();
+                c.c_id = id;
+                c.c_name = name;
+                c.c_age = age;
+                db.condoctors.Add(c);
+                db.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult updateData(abcTest obj)
+        {
+            var con=db.condoctors.Where(model=>model.c_id==obj.c_id).FirstOrDefault();
+            if (con != null)
+            {
+                if (obj.bus_id!=null)
+                {
+                    int bId=int.Parse(obj.bus_id);
+                    var com=db.commons.Where(model=>model.bus_id==bId).FirstOrDefault();
+                    com.bus_id = bId;
+                    db.SaveChanges();
+                }
+           
+                con.c_id = obj.c_id;
+                con.c_name = obj.name;
+                con.c_age = obj.age;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+        public class abcTest
+        {
+            //public int bus_id { get; set; }
+            [NotMapped]
+            public string bus_id { get; set; }
+            public int bus_time { get; set; }
+            public string bus_route { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+            public int c_id { get; set; }
+
+        }
+    }
+}
